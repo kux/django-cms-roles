49eaa0360274747717e746a00975d4ccf9982d68
Alex Cucu <alexandruioan.cucu@gmail.com>
2013-08-01 13:49:55 +0300

    Grant users diferent roles using the dedicated 'grant_to_user'

--- setup.py
+++ setup.py
@@ -13,8 +13,8 @@ setup(
     url='https://github.com/kux/django-cms-roles',
     install_requires=(
         'parse',
-        'Django>=1.3,<1.5',
-        'django-cms>=2.3,<2.4'),
+        'Django>=1.3,<1.5',),
+        # 'django-cms>=2.3,<2.4'),
     setup_requires=[
         's3sourceuploader',
     ],
